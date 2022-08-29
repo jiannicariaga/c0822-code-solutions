d332c476b9a6f40e063781722a2868786796eb76
Jianni Cariaga <jiannicariaga@gmail.com>
2022-08-29 16:24:15 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Jianni Cariaga';
+var isCool = true;
+var totalPets = 2;
+
+console.log('Value of fullName:', fullName);
+console.log('Value of isCool:', isCool);
+console.log('Value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
+
+var unknown = null;
+
+console.log('Value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+
+console.log('Value of never:', never);
+console.log('typeof never:', typeof never);
