62b391c09d596cc8af9045123061b8a6b5f38123
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-01 16:54:26 +0000

    Added a break statement to exit for loop should the array input have more than one 7.

--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -4,6 +4,7 @@
 //    1a) Assign false to storage
 // 2) Check each value in the (array) input
 //    2a) Invert storage if the number 7 is found
+//    2b) Stop checking values once a number 7 is found
 // 3) Output storage
 
 function includesSeven(array) {
@@ -12,6 +13,7 @@ function includesSeven(array) {
   for (var i = 0; i < array.length; i++) {
     if (array[i] === 7) {
       hasSeven = !hasSeven;
+      break;
     }
   }
 
