a62c33cb588439c0379e89df22f5f83744e20965
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-06 18:48:02 +0000

    Updated query selector arguments and updtated console log arguments.

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,26 +1,26 @@
 function handleClick(event) {
   console.log('message:', 'button clicked');
-  console.log('event object:', 'click');
-  console.log('target property:', '.click-button');
+  console.log('event object:', event);
+  console.log('target property:', event.target);
 }
 
-var $buttoneOne = document.querySelector('[class=\'click-button\']');
+var $buttoneOne = document.querySelector('.click-button');
 $buttoneOne.addEventListener('click', handleClick);
 
 function handleMouseover(event) {
   console.log('message:', 'button hovered');
-  console.log('event:', 'mouseover');
-  console.log('target property:', '.hover-button');
+  console.log('event:', event);
+  console.log('target property:', event.target);
 }
 
-var $buttonTwo = document.querySelector('[class=\'hover-button\']');
+var $buttonTwo = document.querySelector('.hover-button');
 $buttonTwo.addEventListener('mouseover', handleMouseover);
 
 function handleDoubleClick(event) {
   console.log('message:', 'button double-clicked');
-  console.log('event:', 'dblclick');
-  console.log('target property:', '.double-click-button');
+  console.log('event:', event);
+  console.log('target property:', event.target);
 }
 
-var $buttonThree = document.querySelector('[class=\'double-click-button\']');
+var $buttonThree = document.querySelector('.double-click-button');
 $buttonThree.addEventListener('dblclick', handleDoubleClick);
