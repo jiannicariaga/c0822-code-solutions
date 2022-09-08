4e6ce565b0ba917eb164ff3d78fbb81426134106
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-08 05:20:43 +0000

    Saving progress.

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,21 @@
 /* exported includes */
+
+// 1) Create storage for boolean output
+//    1a) Assign false to storage
+// 2) Check each element in array input
+//    2a) Assign true to storage if current element matches value input
+//    2b) Stop checking each element in aray input
+// 3) Output storage
+
+function includes(array, value) {
+  var included = false;
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      included = !included;
+      break;
+    }
+  }
+
+  return included;
+}
