fd558553bffe8678f5715f980371bfb044422981
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-08 23:57:00 +0000

    Saving progress.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,17 @@
+// 1) Query first character
+// 2) Store text content of first character
+// 3)
+// 2) Underline first character
+// 3) Check if keydown matches first character
+//    3a) Y: Turn charater green and remove underline
+//        3aa) Move to next character
+//    3b) N: Turn character red
+//        3ba) Stay on current character until keydown matches
+
+// var $phrase = document.querySelector('.phrase');
+// var $characters = document.querySelectorAll('span');
+
+document.addEventListener('keydown', function (event) {
+  // console.log('event.key:', event.key);
+
+});
