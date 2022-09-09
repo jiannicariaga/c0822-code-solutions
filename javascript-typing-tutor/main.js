460805caed93838e3525c8dddc4df9e9d7601b0e
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-09 21:33:32 +0000

    Completed javascript-typing-tutor exercise.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,12 +1,28 @@
-// Insert span elements to HTML file
-// Update CSS
+generatePhrase('grumpy wizards make toxic brew');
 
 var $phrase = document.querySelectorAll('span');
-var $accuracy = document.querySelector('h2');
-var $modal = document.querySelector('.modal-off');
-var $reset = document.querySelector('.reset');
 var index = 0;
 var keystrokes = 0;
+var $modal = document.querySelector('.modal-off');
+var $accuracy = document.querySelector('.accuracy');
+var $reset = document.querySelector('.reset');
+
+function generatePhrase(string) {
+  var $phraseContainer = document.querySelector('h1');
+
+  for (var i = 0; i < string.length; i++) {
+    var $span = document.createElement('span');
+
+    if (i === 0) {
+      $span.setAttribute('class', 'current-char');
+    }
+
+    $span.textContent = string[i];
+    $phraseContainer.appendChild($span);
+  }
+
+  return $phrase;
+}
 
 document.addEventListener('keydown', function (event) {
   keystrokes++;
@@ -14,7 +30,7 @@ document.addEventListener('keydown', function (event) {
   if (index === $phrase.length - 1) {
     event.preventDefault();
     $phrase[index].className = 'correct';
-    gameOver();
+    endPrompt();
     return;
   }
 
@@ -31,6 +47,12 @@ document.addEventListener('keydown', function (event) {
   }
 });
 
+function endPrompt() {
+  var accuracy = ($phrase.length / keystrokes) * 100;
+  $accuracy.textContent = 'Accuracy: ' + accuracy.toFixed(2) + '%';
+  $modal.className = 'modal-on';
+}
+
 $reset.addEventListener('click', function (event) {
   $phrase[0].className = 'current-char';
   $modal.className = 'modal-off';
@@ -38,12 +60,6 @@ $reset.addEventListener('click', function (event) {
   keystrokes = 0;
 
   for (var i = 1; i < $phrase.length; i++) {
-    $phrase[i].className = '';
+    $phrase[i].removeAttribute('class');
   }
 });
-
-function gameOver() {
-  var accuracy = ($phrase.length / keystrokes) * 100;
-  $accuracy.textContent = 'Accuracy: ' + accuracy.toFixed(2) + '%';
-  $modal.className = 'modal-on';
-}
