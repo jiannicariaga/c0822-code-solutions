3a9e08eac3d1aa851e4306db00504919b235938f
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-12 05:58:25 +0000

    Rewrote functions to reduce global variables.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,11 +1,6 @@
-generatePhrase('grumpy wizards make toxic brew');
-
-var $phrase = document.querySelectorAll('span');
+var phrase = generatePhrase('omae wa mou shindeiru');
 var index = 0;
 var keystrokes = 0;
-var $modal = document.querySelector('.modal-off');
-var $accuracy = document.querySelector('.accuracy');
-var $reset = document.querySelector('.reset');
 
 function generatePhrase(string) {
   var $phraseContainer = document.querySelector('h1');
@@ -21,45 +16,52 @@ function generatePhrase(string) {
     $phraseContainer.appendChild($span);
   }
 
+  var $phrase = document.querySelectorAll('span');
+
   return $phrase;
 }
 
 document.addEventListener('keydown', function (event) {
   keystrokes++;
 
-  if (index === $phrase.length - 1) {
+  if (index === phrase.length - 1) {
     event.preventDefault();
-    $phrase[index].className = 'correct';
+    phrase[index].className = 'correct';
     endPrompt();
+
     return;
   }
 
-  if (event.key === $phrase[index].textContent) {
-    $phrase[index].className = 'correct';
+  if (event.key === phrase[index].textContent) {
+    phrase[index].className = 'correct';
     index++;
   } else {
-    $phrase[index].className = 'current-char incorrect';
+    phrase[index].className = 'current-char incorrect';
     return;
   }
 
-  if (index < $phrase.length) {
-    $phrase[index].className = 'current-char';
+  if (index < phrase.length) {
+    phrase[index].className = 'current-char';
   }
 });
 
 function endPrompt() {
-  var accuracy = ($phrase.length / keystrokes) * 100;
-  $accuracy.textContent = 'Accuracy: ' + accuracy.toFixed(2) + '%';
+  var $accuracy = document.querySelector('.accuracy');
+  var percentage = (phrase.length / keystrokes) * 100;
+  var $modal = document.querySelector('.modal-off');
+  var $reset = document.querySelector('.reset');
+
+  $accuracy.textContent = 'Accuracy: ' + percentage.toFixed(2) + '%';
   $modal.className = 'modal-on';
+
+  $reset.addEventListener('click', function (event) {
+    $modal.className = 'modal-off';
+    phrase[0].className = 'current-char';
+    index = 0;
+    keystrokes = 0;
+
+    for (var i = 1; i < phrase.length; i++) {
+      phrase[i].removeAttribute('class');
+    }
+  });
 }
-
-$reset.addEventListener('click', function (event) {
-  $phrase[0].className = 'current-char';
-  $modal.className = 'modal-off';
-  index = 0;
-  keystrokes = 0;
-
-  for (var i = 1; i < $phrase.length; i++) {
-    $phrase[i].removeAttribute('class');
-  }
-});
