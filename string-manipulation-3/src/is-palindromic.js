658b8ead22d0492e80006a83f826508b13981e41
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-16 15:57:17 +0000

    Saving progress.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,30 @@
+/* exported isPalindromic */
+
+// 1) Create storage for boolean output
+// 2) Create (2) temporary storages for arrays
+// 2) Remove spaces from string input
+// 3) Check whether the number of characters in string input is even or odd
+//    3a) Divide string input in halves and assign to temporary storage
+// 4) Check each character in temporary storage
+//    4a) First temporary storage checks from beginning to end
+//    4b) Second temporary storage checks from end to beginning
+// 5) Compare each character in temparary storages
+//    5a) Output 'false' if the current characters from temporary storages do not match
+//    5b) Output 'true' if the current characters from temporary storages match
+
+function isPalindromic(string) {
+  // var palindrome = true;
+  // var tempString = string.split(' ').join('');
+  // var firstHalf = '';
+  // var secondHalf = '';
+
+  // if (string.length % 2 === 0) {
+
+  // } else {
+  //   firstHalf = tempString.substring(0, Math.floor(tempString.length / 2));
+  //   secondHalf = tempString.substring(Math.ceil(tempString.length / 2));
+  // }
+
+  // console.log(firstHalf);
+  // console.log(secondHalf);
+}
