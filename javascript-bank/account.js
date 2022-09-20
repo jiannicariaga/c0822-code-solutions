ba98d03d75a963063c081c9fdfa5cf9bff17f8b1
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-20 18:30:25 +0000

    Completed Account section.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -7,29 +7,43 @@ function Account(number, holder) {
 }
 
 Account.prototype.deposit = function (amount) {
-  var transaction = new Transaction('deposit', amount);
+  var deposit = new Transaction('deposit', amount);
 
   if (isNaN(amount) || !Number.isInteger(amount) || amount <= 0) {
     return false;
   }
 
-  this.transactions.push([transaction.type, transaction.amount]);
+  this.transactions.push(deposit);
 
   return true;
 };
 
 Account.prototype.withdraw = function (amount) {
-  var transaction = new Transaction('withdrawal', amount);
+  var withdrawal = new Transaction('withdrawal', amount);
 
   if (isNaN(amount) || !Number.isInteger(amount) || amount <= 0) {
     return false;
   }
 
-  this.transactions.push([transaction.type, transaction.amount]);
+  this.transactions.push(withdrawal);
 
   return true;
 };
 
 Account.prototype.getBalance = function () {
+  var balance = 0;
 
+  if (!this.transactions.length) {
+    return 0;
+  }
+
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      balance += this.transactions[i].amount;
+    } else {
+      balance -= this.transactions[i].amount;
+    }
+  }
+
+  return balance;
 };
