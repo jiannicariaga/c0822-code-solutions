4dc9bcef4ce335263250679e162b5f37044a0a4d
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-22 17:46:27 +0000

    Completed javascript-ajax exercise.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+var $userList = document.querySelector('#user-list');
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+
+xhr.addEventListener('load', function (event) {
+  console.log(xhr.status);
+  console.log(xhr.response);
+
+  for (var i = 0; i < xhr.response.length; i++) {
+    var listItem = document.createElement('li');
+    listItem.textContent = xhr.response[i].name;
+    $userList.appendChild(listItem);
+  }
+});
+
+xhr.send();
