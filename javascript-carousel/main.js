c90884573422b2f3c69a9db983a2cc0f4474fc83
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-22 04:11:57 +0000

    Added clickable chevrons that will step the images and dots back or forth.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -23,12 +23,16 @@ var images = [
 var imagesIndex = 0;
 var intervalId = null;
 var $heroImage = document.querySelector('.hero-image');
-// var $chevronLeft = document.querySelector('');
-// var $chevronRight = document.querySelector('');
+var $chevronLeft = document.querySelector('.chevron-left');
+var $chevronRight = document.querySelector('.chevron-right');
 var $progressDots = document.querySelector('.progress-dots');
 var dots = $progressDots.children;
 
-function stepCarousel() {
+function startCarousel() {
+  intervalId = setInterval(stepImages, 3000);
+}
+
+function stepImages() {
   imagesIndex++;
 
   if (imagesIndex === images.length) {
@@ -37,7 +41,10 @@ function stepCarousel() {
 
   $heroImage.src = images[imagesIndex].src;
   $heroImage.alt = images[imagesIndex].alt;
+  stepProgressDots();
+}
 
+function stepProgressDots() {
   for (var i = 0; i < images.length; i++) {
     if (i === imagesIndex) {
       dots[i].className = 'dot fas fa-circle';
@@ -47,6 +54,40 @@ function stepCarousel() {
   }
 }
 
+$chevronLeft.addEventListener('click', function (event) {
+  clearInterval(intervalId);
+
+  if (imagesIndex === 0) {
+    imagesIndex = images.length - 1;
+    $heroImage.src = images[imagesIndex].src;
+    $heroImage.alt = images[imagesIndex].alt;
+  } else {
+    imagesIndex--;
+    $heroImage.src = images[imagesIndex].src;
+    $heroImage.alt = images[imagesIndex].alt;
+  }
+
+  stepProgressDots();
+  startCarousel();
+});
+
+$chevronRight.addEventListener('click', function (event) {
+  clearInterval(intervalId);
+
+  if (imagesIndex === images.length - 1) {
+    imagesIndex = 0;
+    $heroImage.src = images[imagesIndex].src;
+    $heroImage.alt = images[imagesIndex].alt;
+  } else {
+    imagesIndex++;
+    $heroImage.src = images[imagesIndex].src;
+    $heroImage.alt = images[imagesIndex].alt;
+  }
+
+  stepProgressDots();
+  startCarousel();
+});
+
 $progressDots.addEventListener('click', function (event) {
   var dotClicked;
 
@@ -55,19 +96,19 @@ $progressDots.addEventListener('click', function (event) {
   if (event.target.matches('.dot')) {
     dotClicked = event.target.getAttribute('data-dot-id');
 
-    for (var i = 0; i < dots.length; i++) {
+    for (var i = 0; i < images.length; i++) {
       if (dots[i].getAttribute('data-dot-id') === dotClicked) {
-        dots[i].className = 'dot fas fa-circle';
         imagesIndex = i;
-        $heroImage.src = images[imagesIndex].src;
-        $heroImage.alt = images[imagesIndex].alt;
+        $heroImage.src = images[i].src;
+        $heroImage.alt = images[i].alt;
+        dots[i].className = 'dot fas fa-circle';
       } else {
         dots[i].className = 'dot far fa-circle';
       }
     }
   }
 
-  intervalId = setInterval(stepCarousel, 3000);
+  startCarousel();
 });
 
-intervalId = setInterval(stepCarousel, 3000);
+startCarousel();
