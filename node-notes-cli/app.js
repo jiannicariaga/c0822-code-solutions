46de69f7da77aafed63c351cbe4344034ce7d6a4
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-13 00:01:14 +0000

    Saving progress to transfer machines.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,7 +1,21 @@
-const object = require('./data.json');
+const data = require('./data.json');
+// const fs = require('fs');
 
 if (process.argv[2] === 'read') {
+  read(data);
+} else if (process.argv[2] === 'add') {
+  add(data);
+}
+
+function read(object) {
   for (const property in object.notes) {
     console.log(`${property}: ${object.notes[property]}`);
   }
+}
+
+function add(object) {
+  // console.log(object.nextId);
+  // console.log(process.argv[3]);
+  data.notes[data.nextId] = process.argv[3];
+  // console.log(data.notes[data.nextId] = process.argv[3]);
 }
