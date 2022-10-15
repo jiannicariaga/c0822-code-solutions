83e1c2b51d7a47c2ec7ac16aeb3fbb78a3c775b5
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-15 21:07:55 +0000

    Removed redundant code for successful get by id operation.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -21,12 +21,8 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404);
     res.json({ error: `cannot find note with id ${req.params.id}.` });
   } else {
-    for (const id in data.notes) {
-      if (req.params.id === id) {
-        res.status(200);
-        res.send(data.notes[id]);
-      }
-    }
+    res.status(200);
+    res.send(data.notes[req.params.id]);
   }
 });
 
