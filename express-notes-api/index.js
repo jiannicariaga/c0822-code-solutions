f9a78a0535ce0d6eb361aa8c531e9752bc537cc6
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-15 18:19:44 +0000

    Completed create operations.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const data = require('./data.json');
 const express = require('express');
 const app = express();
-const data = require('./data.json');
+
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
   const list = [];
@@ -13,10 +16,10 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   if (!Number.isInteger(Number(req.params.id))) {
     res.status(400);
-    res.json({ error: 'id must be a positive integer' });
+    res.json({ error: 'id must be a positive integer.' });
   } else if (!(req.params.id in data.notes)) {
     res.status(404);
-    res.json({ error: `cannot find note with id ${req.params.id}` });
+    res.json({ error: `cannot find note with id ${req.params.id}.` });
   } else {
     for (const id in data.notes) {
       if (req.params.id === id) {
@@ -24,6 +27,28 @@ app.get('/api/notes/:id', (req, res) => {
         res.send(data.notes[id]);
       }
     }
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  if (req.body === undefined) {
+    res.status(400);
+    res.json({ error: 'content is a required field.' });
+  } else {
+    const newNote = req.body;
+    newNote.id = data.nextId;
+    data.notes[data.nextId] = newNote;
+    data.nextId++;
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+      if (err) {
+        console.error(err);
+        res.status(500);
+        res.json({ error: 'an unexpected error has occured.' });
+      } else {
+        res.status(201);
+        res.json(req.body);
+      }
+    });
   }
 });
 
