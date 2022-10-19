8acafd37e9d79e49398d2bb9db0e5d2f55f7a81f
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-19 17:53:52 +0000

    Added create operation.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -24,6 +24,35 @@ app.get('/api/grades', (req, res) => {
     });
 });
 
+app.post('/api/grades', (req, res) => {
+  const course = req.body.course;
+  const name = req.body.name;
+  const score = req.body.score;
+  if (!course || !name || !score) {
+    res.status(400).json({ error: 'course, name, and score are required fields.' });
+    return;
+  }
+  if (!Number.isInteger(score) || score < 0 || score > 100) {
+    res.status(400).json({ error: 'score must be a integer between 0 and 100.' });
+    return;
+  }
+  const sql = `
+    insert into "grades" ("course", "name", "score")
+    values ($1, $2, $3)
+    returning *;
+  `;
+  const params = [course, name, score];
+  db.query(sql, params)
+    .then(result => {
+      const newGrade = result.rows[0];
+      res.status(201).json(newGrade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occured.' });
+    });
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000.');
