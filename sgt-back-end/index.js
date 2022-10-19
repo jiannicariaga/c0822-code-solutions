aa03f444c7d8b2b48e6281765b34990c05a166ff
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-19 19:06:58 +0000

    Added update operation.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -20,7 +20,7 @@ app.get('/api/grades', (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'An unexpected error occured.' });
+      res.status(500).json({ error: 'an unexpected error occured.' });
     });
 });
 
@@ -33,7 +33,7 @@ app.post('/api/grades', (req, res) => {
     return;
   }
   if (!Number.isInteger(score) || score < 0 || score > 100) {
-    res.status(400).json({ error: 'score must be a integer between 0 and 100.' });
+    res.status(400).json({ error: 'score must be an integer between 0 and 100.' });
     return;
   }
   const sql = `
@@ -46,6 +46,47 @@ app.post('/api/grades', (req, res) => {
     .then(result => {
       const newGrade = result.rows[0];
       res.status(201).json(newGrade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'an unexpected error occured.' });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  const course = req.body.course;
+  const name = req.body.name;
+  const score = Number(req.body.score);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({ error: 'gradeId must be a positive integer.' });
+    return;
+  }
+  if (!course || !name || !score) {
+    res.status(400).json({ error: 'course, name, and score are required fields.' });
+    return;
+  }
+  if (!Number.isInteger(score) || score < 0 || score > 100) {
+    res.status(400).json({ error: 'score must be an integer between 0 and 100.' });
+    return;
+  }
+  const sql = `
+    update "grades"
+       set "course"  = $2,
+           "name"    = $3,
+           "score"   = $4
+     where "gradeId" = $1
+     returning *;
+  `;
+  const params = [gradeId, course, name, score];
+  db.query(sql, params)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({ error: `cannot find grade with gradeId ${gradeId}.` });
+      } else {
+        res.json(grade);
+      }
     })
     .catch(err => {
       console.error(err);
