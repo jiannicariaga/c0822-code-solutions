627935944c6aba48bd05700b6a7f70e822c0f95a
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-20 18:00:19 +0000

    Added a function defenition that mimics the filter method.

---
+++ array-filter/filter.js
@@ -0,0 +1,13 @@
+function isEven(number) {
+  return !(number % 2);
+}
+
+function filter(array, predicate) {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
