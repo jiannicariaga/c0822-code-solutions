438b6ad1bb2234a0925da8eb31847b701828a904
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-25 23:33:52 +0000

    Made minimal render of stopwatch.

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export class StopWatch extends React.Component {
-
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-100'>
+            <div clasName='stop-watch'>
+              <h1>0</h1>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-100 '>
+            <span className="fa-solid fa-play"></span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
