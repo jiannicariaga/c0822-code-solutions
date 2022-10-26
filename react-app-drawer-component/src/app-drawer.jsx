0b0d888cd3614f6727a890a5d42ae79d7fbcdc18
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-26 21:56:10 +0000

    Rendered menu and overlad.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -3,12 +3,35 @@ import React from 'react';
 export class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { drawerOpen: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ drawerOpen: !this.state.drawerOpen });
   }
 
   render() {
+    let overlay;
+    let drawer;
+
+    if (this.state.drawerOpen) {
+      overlay = 'overlay-open';
+      drawer = 'drawer-open';
+    } else {
+      overlay = 'overlay-closed';
+      drawer = 'drawer-closed';
+    }
+
     return (
-      <div></div>
+      <div className={overlay}>
+        <div className={drawer}>
+          <h3>Menu</h3>
+          <a href="#" onClick={this.handleClick}>About</a>
+          <a href="#" onClick={this.handleClick}>Get Started</a>
+          <a href="#" onClick={this.handleClick}>Sign Up</a>
+        </div>
+      </div>
     );
   }
 }
