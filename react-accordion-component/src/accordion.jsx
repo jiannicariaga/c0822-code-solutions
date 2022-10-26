cd00d4cd8927181b62dc3130f1a89802f5dd39fa
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-26 23:32:25 +0000

    Added starting code.

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
