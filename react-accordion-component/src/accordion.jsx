805f5593e23edee2e3d1057aa9d8cd1e217580b9
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-28 22:06:08 +0000

    Saving progress to transfer machines.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openTopic: null
+    };
+    this.toggleContent = this.toggleContent.bind(this);
+    this.buildAccordion = this.buildAccordion.bind(this);
+  }
+
+  buildAccordion(props) {
+
+  }
+
   toggleContent(event) {
     const accordionItems = document.querySelectorAll('.label');
     const openAccordionItem = document.querySelector('.open');
@@ -21,42 +34,21 @@ export default class Accordion extends React.Component {
   }
 
   render() {
+    const topics = this.props.topics;
+    const accordionItems = topics.map(topic => {
+      return (
+        <div className='accordion-item' key={topic.label}>
+          <h3 className='label' onClick={this.buildAccordion}>{topic.label}</h3>
+          <div className='content'>
+            <p>{topic.content}</p>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div className='accordion-wrapper'>
-        <div className='accordion-item'>
-          <h3 className='label' onClick={this.toggleContent}>Hypertext Markup Language</h3>
-          <div className='content'>
-            <p>
-              Hypertext Markup Language (HTML) is the standard markup Language
-              for creating web pages and web applications. With Cascading Style
-              Sheets (CSS) and JavaScript, it forms a triad of cornerstone
-              technologies for the World Wide Web.
-            </p>
-          </div>
-        </div>
-        <div className='accordion-item'>
-          <h3 className='label' onClick={this.toggleContent}>Cascading Style Sheets</h3>
-          <div className='content'>
-            <p>
-              Cascading Style Sheets (CSS) is a style sheet language used for
-              describing the presentation of a document written in a markup
-              language like HTML. CSS is a cornerstone technology of the World
-              Wide Web alongside HTML and JavaScript.
-            </p>
-          </div>
-        </div>
-        <div className='accordion-item'>
-          <h3 className='label' onClick={this.toggleContent}>JavaScript</h3>
-          <div className='content'>
-            <p>
-              JavaScript, often abbreviated as JS, is a high-level, interpreted
-              programming language that conforms to the ECMAScript
-              specification. JavaScript has curly-bracket syntax, dynamic
-              typing, prototype-based object-orientation, and first-class
-              functions.
-            </p>
-          </div>
-        </div>
+        {accordionItems}
       </div>
     );
   }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Accordion from './accordion';
 
+const topics = [
+  {
+    label: 'Hypertext Markup Language',
+    content: `Hypertext Markup Language (HTML) is the standard markup Language
+              for creating web pages and web applications.With Cascading Style
+              Sheets (CSS) and JavaScript, it forms a triad of cornerstone
+              technologies for the World Wide Web.`
+  },
+  {
+    label: 'Cascading Style Sheets',
+    content: `Cascading Style Sheets (CSS) is a style sheet language used for
+              describing the presentation of a document written in a markup
+              language like HTML. CSS is a cornerstone technology of the World
+              Wide Web alongside HTML and JavaScript.`
+  },
+  {
+    label: 'JavaScript',
+    content: `JavaScript, often abbreviated as JS, is a high-level, interpreted
+              programming language that conforms to the ECMAScript
+              specification. JavaScript has curly-bracket syntax, dynamic
+              typing, prototype-based object-orientation, and first-class
+              functions.`
+  }
+];
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<Accordion />);
+root.render(<Accordion topics={topics} />);
