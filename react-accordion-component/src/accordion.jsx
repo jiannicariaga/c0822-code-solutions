d3318823fd2642c9eb8d2a3c20aa0f90dc2b7dd9
Jianni Cariaga <jiannicariaga@gmail.com>
2022-10-29 19:38:24 +0000

    Completed react-accordion-component challenge.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,42 +3,28 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      openTopic: null
-    };
-    this.toggleContent = this.toggleContent.bind(this);
-    this.buildAccordion = this.buildAccordion.bind(this);
+    this.state = { openTopic: null };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  buildAccordion(props) {
-
-  }
-
-  toggleContent(event) {
-    const accordionItems = document.querySelectorAll('.label');
-    const openAccordionItem = document.querySelector('.open');
-
-    accordionItems.forEach(accordionItem => {
-      if (accordionItem.classList.contains('open')) {
-        accordionItem.classList.remove('open');
-      }
-    });
-
-    if (!event.target.classList.contains('open')) {
-      event.target.classList.add('open');
-    }
-
-    if (event.target === openAccordionItem) {
-      event.target.classList.remove('open');
+  handleClick(event) {
+    const label = event.target.textContent;
+    if (label === this.state.openTopic) {
+      this.setState({ openTopic: null });
+    } else {
+      this.setState({ openTopic: label });
     }
   }
 
   render() {
     const topics = this.props.topics;
     const accordionItems = topics.map(topic => {
+      const display = this.state.openTopic === topic.label ? 'open' : '';
       return (
         <div className='accordion-item' key={topic.label}>
-          <h3 className='label' onClick={this.buildAccordion}>{topic.label}</h3>
+          <h3 className={`label ${display}`} onClick={this.handleClick}>
+            {topic.label}
+          </h3>
           <div className='content'>
             <p>{topic.content}</p>
           </div>
