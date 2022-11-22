e84f0cc9ff53dc757b25590e17539ad56823b78a
Jianni Cariaga <jiannicariaga@gmail.com>
2022-11-22 23:56:43 +0000

    Removed additional data structure.

--- data-structures-stacks/count-values.js
+++ data-structures-stacks/count-values.js
@@ -2,11 +2,10 @@
 
 function countValues(stack) {
   let count = 0;
-  const values = [];
   if (stack.peek() === undefined) return count;
   while (stack.peek() !== undefined) {
-    values.push(stack.pop());
+    stack.pop();
     count++;
   }
-  return values.length;
+  return count;
 }
