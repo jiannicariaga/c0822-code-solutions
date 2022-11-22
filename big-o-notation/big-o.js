9237776bdcb31cf24f6c3362aad1e71d89bd4f8c
Jianni Cariaga <jiannicariaga@gmail.com>
2022-11-22 00:07:44 +0000

    Updated comments.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -8,10 +8,10 @@ function uniqueLinear(words) {
     i < words.length;               // 2 * n = O(n)
     i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 1 * n = O(n)
-    if (!seen[word]) {              // 1 * n = O(n)
-      seen[word] = true;            // 1 * n = O(n)
-      unique[unique.length] = word; // 1 * n = O(n)
+    const word = words[i];          // 2 * n = O(n)
+    if (!seen[word]) {              // 3 * n = O(n)
+      seen[word] = true;            // 2 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
@@ -24,20 +24,20 @@ function uniqueQuadratic(words) {
     i < words.length;               // 2 * n = O(n)
     i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 1 * n = O(n)
+    const word = words[i];          // 2 * n = O(n)
     let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 2 * n = O(n)
-      c++                           // 2 * n = O(n)
+      c < i;                        // 2 * n * n = O(n^2)
+      c++                           // 2 * n * n = O(n^2)
     ) {
-      const comparing = words[c];   // 1 * n^2 = O(n^2)
-      if (comparing === word) {     // 1 * n^2 = O(n^2)
-        isUnique = false;           // 1 * n^2 = O(n^2)
+      const comparing = words[c];   // 2 * n * n = O(n^2)
+      if (comparing === word) {     // 2 * n * n = O(n^2)
+        isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * n^2 = O(n^2)
-      unique[unique.length] = word; // 1 * n^2 = O(n^2)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
