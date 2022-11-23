61e6787a07e54b43a1ad6b9005660178e9b33554
Jianni Cariaga <jiannicariaga@gmail.com>
2022-11-23 22:40:38 +0000

    Completed javascript-closures exercise.

---
+++ javascript-closures/main.js
@@ -0,0 +1,11 @@
+function graduate(credential) {
+  return function (fullName) {
+    return `${fullName}, ${credential}`;
+  };
+}
+
+const medicalSchool = graduate('M.D.');
+const lawSchool = graduate('Esq.');
+
+console.log(medicalSchool('Anthony Urbina'));
+console.log(lawSchool('(The Real) Anthony Urbina'));
